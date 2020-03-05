c7817afec0b0e23a006a257c63bbff51f04a4a4f
Hlomla <hlomlakwezi00@gmail.com>
2020-03-05 22:14:05 +0200

    Complete media query for the entire website

--- public/js/index.js
+++ public/js/index.js
@@ -58,7 +58,7 @@ $(document).ready(function(){
     // open menu toggle
 
     $('.open-toogle').on('click',function(){
-        $('.nav-list').css({
+        $('.nav-mobile').css({
             display:'block'
         })
         $('.close-toogle').css({
@@ -69,7 +69,7 @@ $(document).ready(function(){
         })
     })
     $('.nav-list__items').click(function(){
-        $('.nav-list').css({
+        $('.nav-mobile').css({
             display:'none'
         })
         $('.open-toogle').css({
@@ -81,7 +81,7 @@ $(document).ready(function(){
     })
 
     $('.close-toogle').click(function(){
-        $('.nav-list').css({
+        $('.nav-mobile').css({
             display:'none'
         })
         $('.open-toogle').css({
